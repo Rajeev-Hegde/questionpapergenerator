f041ca0697375013497e09fc370a70fac4a860e9
Rajeev-Hegde <rajeevh.93@gmail.com>
2019-08-12 17:44:45 +0530

    Project init

---
+++ src/main/java/com/systemdesign/questionpapergenerator/QuestionpapergeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.systemdesign.questionpapergenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionpapergeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionpapergeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/systemdesign/questionpapergenerator/QuestionpapergeneratorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.systemdesign.questionpapergenerator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuestionpapergeneratorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
